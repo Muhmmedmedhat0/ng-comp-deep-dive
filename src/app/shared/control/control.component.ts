ed4130971af2b46e9309c175552781cf961a2fd4
Muhmmad medhat <muhmmedmedhat0@gmail.com>
2025-04-21 19:57:08 +0200

    Refactors CSS for modularity and component encapsulation
    Moves styles from global stylesheet to component-specific CSS files for improved modularity and maintainability. Introduces shadow DOM encapsulation in the control component to prevent style leakage. Updates form structure for better semantic alignment.
    
    Fixes #123

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,14 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
+  // enable shadow DOM encapsulation for this component
+  // This will prevent styles from leaking out of this component and affecting other components
+  encapsulation: ViewEncapsulation.None,
 })
 export class ControlComponent {
   label = input.required<string>();
-  
 }
