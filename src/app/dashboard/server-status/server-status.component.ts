9570839b91cf628962fe6c93f536010ec2d4938b
Muhmmad medhat <muhmmedmedhat0@gmail.com>
2025-04-24 17:29:41 +0200

    Improves server status component styling and logic
    Replaces CSS selector with a more robust :host usage.
    Enhances HTML structure with dynamic class binding for status.
    Refines TypeScript logic to randomize server status periodically.
    
    Improves code maintainability and dynamic behavior.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-server-status',
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const random = Math.random();
+      this.currentStatus = random > 0.5 ? 'online' : random > 0.9 ? 'offline' : 'unknown';
+    }, 5000);
+  }
 }
