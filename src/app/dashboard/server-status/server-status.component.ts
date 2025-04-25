ff7fdc66a5378ebcd6da1d10cbb9c2a13e95c0d1
Muhmmad medhat <muhmmedmedhat0@gmail.com>
2025-04-25 19:16:24 +0300

    Implements OnInit lifecycle hook in server status component
    Implements OnInit lifecycle in server status component
    
    Adds OnInit interface to manage component initialization.
    Introduces periodic status updates using setInterval in ngOnInit.
    
    Improves status handling logic for better readability.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,12 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
   ngOnInit() {
     setInterval(() => {
-      const random = Math.random();
+      const random = Math.random(); // random number between 0 and 0.9999
       this.currentStatus =
         random > 0.5 ? 'online' : random > 0.9 ? 'offline' : 'unknown';
     }, 5000);
