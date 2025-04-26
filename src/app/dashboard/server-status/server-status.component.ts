269daabec1d27f4ef63ecc513894a5d197df7c5f
Muhmmad medhat <muhmmedmedhat0@gmail.com>
2025-04-26 17:35:40 +0300

    Adds cleanup logic to prevent memory leaks
    Implements OnDestroy lifecycle hook to clear intervals created in OnInit.
    Prevents potential memory leaks by ensuring intervals are properly cleaned up.

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,14 +6,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval: ReturnType<typeof setInterval> | undefined;
 
   ngOnInit() {
-    setInterval(() => {
+    this.interval = setInterval(() => {
       const random = Math.random(); // random number between 0 and 0.9999
       this.currentStatus =
         random > 0.5 ? 'online' : random > 0.9 ? 'offline' : 'unknown';
     }, 5000);
+  }
+
+  ngOnDestroy() {
+    // Cleanup logic for the interval
+    clearInterval(this.interval);
   }
 }
